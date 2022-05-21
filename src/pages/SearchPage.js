17677a446bc5cc71c75e118cf7a63f2d1e1d2c9e
alanacalvo <alana.keeton@gmail.com>
2022-05-21 18:21:58 -0400

    search page started

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Buffer } from 'buffer';
+import '../searchpage.css'
 
 function SearchPage() {
-
-
-
   const client_id = '638824d8d1cf48bca579d7fa24c5ac40';
   const client_secret = 'cb118a82d49d4d51b6f2e5ecefed9085';
 
   const [token, setToken] = useState("")
   const [searchKey, setSearchKey] = useState('')
   const [artists, setArtists] = useState([])
+  // const [albums, setAlbums] = useState([])
+  // const [songs, setSongs] = useState([])
 
   useEffect(() => {
     async function fetchData() {
@@ -28,7 +28,7 @@ function SearchPage() {
     fetchData();
   }, [])
 
-  const searchSpotify = async (e) => {
+  const searchArtist = async (e) => {
     e.preventDefault()
     const { data } = await axios.get('https://api.spotify.com/v1/search', {
       headers: {
@@ -39,63 +39,35 @@ function SearchPage() {
         type: 'artist'
       }
     })
+    console.log(data)
     setArtists(data.artists.items)
   }
 
-  //     const searchAlbums = async () => {
-  //         e.preventDefault()
-  //         const {data} = await axios.get("https://api.spotify.com/v1/search", { 
-  //             headers: { 
-  //                 Authorization: `Bearer ${token}`
-  //             },
-  //             params: {
-  //                 q: searchKey,
-  //                 type: "album"
-  //             }
-  //         })
-  //         console.log(data)
-  //         setAlbums(data.albums.items)
-  //     }
-  //     searchAlbums()
-  // }
+      // const searchAlbums = async (e) => {
+      //     e.preventDefault()
+      //     const {data} = await axios.get("https://api.spotify.com/v1/search", { 
+      //         headers: { 
+      //             Authorization: `Bearer ${token}`
+      //         },
+      //         params: {
+      //             q: searchKey,
+      //             type: "album"
+      //         }
+      //     })
+      //     console.log(data)
+      //     setAlbums(data.albums.items)
+      // }
 
-  const renderArtists = () => {
-    artists.map((artist, i) => {
-      {
-        artists ? artists.map(artist => {
-          return (
-            <div key={i}>
-
-              <h1>{artist.name}</h1>
-              <h3>{artist.followers.total}</h3>
-              <a href={artist.external_urls.spotify} >Link to page</a>
-              <br></br>
-              {
-                artist.images.length ? <img width={"300px"} src={artist.images[0].url} alt='' />
-                  : <div>No Images</div>
-              }
-              {console.log(artist)}
-            </div>
-          )
-        })
-          : <h2>Search for artist</h2>
-      }
-    })
-  }
-
-
-
-
+  
 
   return (
     <div>
-        <h1>SPOTIFYYY!!!!</h1>
-      <form onSubmit={searchSpotify}>
-        <input type="text" placeholder="Search for artist" onChange={e => setSearchKey(e.target.value)} />
-        <button type={"submit"} >Search</button>
+      <div className="searchContainer">
+      <form  onSubmit={searchArtist}>
+        <input className='searchBar' type="text" placeholder="Search for artist" onChange={e => setSearchKey(e.target.value)} />
+        <button className="searchButton" type={"submit"} >Search</button>
       </form>
-      {renderArtists()}
-      {/* {artists && <h1>{artists[0].name}</h1>} */}
+      </div>
       {artists ? artists.slice(0, 3).map(artist => {
         return (
           <div>
@@ -107,7 +79,7 @@ function SearchPage() {
               artist.images.length ? <img width={"300px"} src={artist.images[0].url} alt='' />
                 : <div>No Images</div>
             }
-            {console.log(artist)}
+
           </div>
         )
       })
