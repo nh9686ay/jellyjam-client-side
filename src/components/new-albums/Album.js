0fa1a5e7a7d8b3a1d2d105ecc11559eb89632ab6
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-05-23 00:45:58 -0400

    made carousel, for albums

--- src/components/new-albums/Album.js
+++ src/components/new-albums/Album.js
@@ -1,27 +1,49 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import '../../itemCarousel.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
 
 
 function Album({ albums }) {
+
+
+    const settings = {
+        dots: true,
+        fade: false,
+        infinite: true,
+        speed:400,
+        slidesToShow: 4,
+        arrows: true,
+        slidesToStroll: 1,
+        className: "slides"
+    }
+
+
   return (
     <div>
+
+        {/* <div > */}
         <div className="new-albums">
+            <Slider {...settings}>
+            
                 {
-                    albums.slice(0, 4).map((album, i) => {
+                    albums.slice(0, 10).map((album, i) => {
                         return (
                             //link to album page by id
-                            <Link to={'/#'} key={i}>
+                            <Link to={'/#'} key={i} className="link">
+                                <h2>{album.name}</h2>
                                 <div key={i} className="album" >
-                                    <h2>{album.name}</h2>
-                                    {album.images.length && <img src={album.images[1].url} />}
+                                    {album.images.length && <img className="album-img" src={album.images[1].url} />}
                                 </div>
                             </Link>
-                            
-                        )
+
+                    )
                     })
                 }
-            </div>
+            </Slider>
+        </div>
     </div>
   )
 }
