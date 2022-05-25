f8abe245b16651a466c0c875cdaf962019b726cf
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-05-25 13:26:58 -0400

    setup signup form value goes to useState

---
+++ src/pages/SignUp.js
@@ -0,0 +1,51 @@
+import React, { useState }from 'react'
+
+function SignUp() {
+
+    const formStructure = {
+        email: '',
+        userName: '',
+        password: '',
+    }
+
+    const [signUpForm, setSignUpForm] = useState(formStructure)
+    const [signUpData, setSignUpData] = useState([])
+
+    const handleSignUpChange = (e) => {
+        setSignUpForm({ ...signUpForm, [e.target.id]: e.target.value })
+    }
+    const handleSignUpSubmit = async (e) => {
+        e.preventDefault()
+        const dataCopy = [...signUpData]
+        dataCopy.push(signUpForm)
+        await setSignUpData(dataCopy)
+        console.log(signUpForm)
+        //then send signUpData to api server
+    }
+
+    if(signUpData.length) {
+        console.log(signUpData)
+        //axios post here to server
+    }
+
+    return (
+        <div>
+            <h1>SignUp</h1>
+
+            <form onSubmit={handleSignUpSubmit} onChange={handleSignUpChange}>
+                <label htmlFor="email">Email: </label>
+                <input type="text" id="email" defaultValue={signUpForm.email} ></input>
+                
+                <label htmlFor="userName">User Name: </label>
+                <input type="text" id="userName" defaultValue={signUpForm.userName} ></input>
+                
+                <label htmlFor="password">Password: </label>
+                <input type="text" id="password" defaultValue={signUpForm.password} ></input>
+                <button type="submit">Sign Up</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default SignUp
