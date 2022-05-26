66a1954f09d6afac9be1ed90f2d3011fec9bc11f
Emon <89279974+nh9686ay@users.noreply.github.com>
2022-05-25 19:05:39 -0500

    navbar+background

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, {useState } from 'react';
 import { Link } from 'react-router-dom';
 import SideNav from '../components/SideNav';
 import NewAlbums from '../components/carousels/new-albums/NewAlbums';
 import FeaturedPlaylists from '../components/carousels/featured-playlists/FeaturedPlaylists';
 
 function HomePage() {
+  const [toggleMenu, setToggleMenu] = useState("");
+
+  const toggleMobileMenu = function(){
+    setToggleMenu(toggleMenu == "" ? "active" : "");
+  };
+
   return (
     <div>
-
-        <h1>JellyJam</h1>
-        <div className="search-container">
-            <Link to="/search">
-            <button>Search</button>
-            </Link>
+        <div className="navWrap">
+            <div className="logo-section">
+                <h3>JellyJam</h3>
+                {/* <div className="search-container">
+                    <Link to="/search">
+                    <button>Search</button>
+                    </Link>
+                </div> */}
+                <div className="mobile_menu" onClick={toggleMobileMenu}>
+                    <div className="dash_line"></div>
+                    <div className="dash_line"></div>
+                    <div className="dash_line"></div>
+                </div>
+            </div>
+            <SideNav toggleMenu={toggleMenu} />
         </div>
-
-        <SideNav />
-        <div>
+        <div className="mainSection">
             <NewAlbums />
-        </div>
-        <div>
             <FeaturedPlaylists />
         </div>
-        
     </div>
   )
 }
